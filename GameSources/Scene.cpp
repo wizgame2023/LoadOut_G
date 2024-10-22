6f9b92d7f5a53f458224841abae8567b2864839b
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-10-22 11:00:44 +0900

    テクスチャ追加

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -14,6 +14,14 @@ namespace basecross{
 	//--------------------------------------------------------------------------------------
 	void Scene::OnCreate(){
 		try {
+			auto& app = App::GetApp();
+
+			auto path = app->GetDataDirWString();
+			auto texPath = path + L"Textures/";
+
+			wstring strTexture = texPath + L"Black.jpg";
+			app->RegisterTexture(L"Black", strTexture);
+
 			//クリアする色を設定
 			Col4 Col;
 			Col.set(31.0f / 255.0f, 30.0f / 255.0f, 71.0f / 255.0f, 255.0f / 255.0f);
@@ -21,6 +29,8 @@ namespace basecross{
 			//自分自身にイベントを送る
 			//これにより各ステージやオブジェクトがCreate時にシーンにアクセスできる
 			PostEvent(0.0f, GetThis<ObjectInterface>(), GetThis<Scene>(), L"ToGameStage");
+
+
 		}
 		catch (...) {
 			throw;
@@ -33,7 +43,7 @@ namespace basecross{
 	void Scene::OnEvent(const shared_ptr<Event>& event) {
 		if (event->m_MsgStr == L"ToGameStage") {
 			//最初のアクティブステージの設定
-			ResetActiveStage<GameStage>();
+			ResetActiveStage<YuutaStage>();
 		}
 	}
 
