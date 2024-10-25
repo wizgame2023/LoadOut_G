b3a886b2fe05d9ae106e5e0e67fc562ad913a345
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-10-25 12:12:10 +0900

    開発ステージの更新

--- GameSources/YuutaStage.cpp
+++ GameSources/YuutaStage.cpp
@@ -12,7 +12,7 @@ namespace basecross {
 	//	ゲームステージクラス実体
 	//--------------------------------------------------------------------------------------
 	void YuutaStage::CreateViewLight() {
-		const Vec3 eye(0.0f, 100.0f, -100.0f);
+		const Vec3 eye(0.0f, 200.0f, -15.0f);
 		const Vec3 at(0.0f);
 		auto PtrView = CreateView<SingleView>();
 		//ビューのカメラの設定
@@ -36,13 +36,16 @@ namespace basecross {
 		catch (...) {
 			throw;
 		}
+		//マップマネージャーの生成
+		auto mapManager = AddGameObject<MapManager>();
+		SetSharedGameObject(L"MapManager", mapManager);
 
 		//床の作成
 		AddGameObject<Ground>();
 		//ブロックの作成
 		for (int i = 0; i < 10; i++)
 		{
-			AddGameObject<Block>(Vec3(-45.0f+(i*10), 5.0f, 50.0f), Vec3(0.0f, 0.0f, 0.0f));
+			AddGameObject<Block>(Vec3(-45.0f+(i*10), 5.0f, 45.0f), Vec3(0.0f, 0.0f, 0.0f));
 
 		}
 		//Playerの生成
