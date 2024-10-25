702b17e9ba0a20da025abc0ae340fa7f42b43758
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-10-25 13:49:50 +0900

    アイテムに触れた際にカウントがプラスする

--- GameSources/Item.h
+++ GameSources/Item.h
@@ -10,13 +10,17 @@ namespace basecross {
 	class Item :public GameObject
 	{
 	private:
+		Vec3 m_pos;//ƒ|ƒWƒVƒ‡ƒ“
+		Vec3 m_rot;//‰ñ“]
 
 	public:
-		Item(shared_ptr<Stage>& stagePtr);
+		Item(shared_ptr<Stage>& stagePtr,Vec3 pos,Vec3 rot);
 		~Item();
 
 		void OnCreate()override;
 		void OnUpdate()override;
+
+		void OnCollisionEnter(shared_ptr<GameObject>& other)override;
 	};
 
 
