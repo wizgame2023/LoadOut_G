2d9315384e1c8b55e405dcb43869c315a7588365
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-10-25 15:21:30 +0900

    開発ステージの更新　オブジェクトの追加、カメラ位置変更

--- GameSources/YuutaStage.cpp
+++ GameSources/YuutaStage.cpp
@@ -12,7 +12,7 @@ namespace basecross {
 	//	ゲームステージクラス実体
 	//--------------------------------------------------------------------------------------
 	void YuutaStage::CreateViewLight() {
-		const Vec3 eye(0.0f, 200.0f, -15.0f);
+		const Vec3 eye(0.0f, 100.0f, -100.0f);
 		const Vec3 at(0.0f);
 		auto PtrView = CreateView<SingleView>();
 		//ビューのカメラの設定
@@ -48,6 +48,9 @@ namespace basecross {
 			AddGameObject<Block>(Vec3(-45.0f+(i*10), 5.0f, 45.0f), Vec3(0.0f, 0.0f, 0.0f));
 
 		}
+		
+		//アイテムの生成
+		AddGameObject<Item>(Vec3(15.0f,2.5f,0.0f),Vec3(0.0f,0.0f,0.0f));
 		//Playerの生成
 		AddGameObject<Player>(Vec3(0.0f, 5.0f, 0.0f), Vec3(0.0f, 0.0f, 0.0f));
 	}
