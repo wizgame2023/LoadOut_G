c1d5f8df8a186e28dabad370712c2adc6db4d37f
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-10-28 14:46:15 +0900

    マンホールの中身作成

--- GameSources/Manhole.cpp
+++ GameSources/Manhole.cpp
@@ -7,7 +7,56 @@
 #include "Project.h"
 
 namespace basecross {
+	Manhole::Manhole(shared_ptr<Stage>& stagePtr,Vec3 pos) :
+		GameObject(stagePtr),
+		m_pos(pos)
+	{
 
+	}
+	Manhole::~Manhole()
+	{
+	}
+
+	void Manhole::OnCreate()
+	{
+		auto ptr = GetComponent<Transform>();
+		ptr->SetPosition(m_pos);
+		ptr->SetRotation(0.0f,0.0f,0.0f);
+		ptr->SetScale(3.0f, 1.0f, 3.0f);
+
+		//Transformに対しての等差数列
+		Mat4x4 spanMat;
+		spanMat.affineTransformation(
+			Vec3(1.0f, 1.0f, 1.0f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(0.0f, 0.0f, 0.0f)
+		);
+
+		//メッシュ生成
+		auto ptrDraw = AddComponent<PNTStaticDraw>();
+		ptrDraw->SetMeshResource(L"DEFAULT_CUBE");
+
+		ptrDraw->SetMeshToTransformMatrix(spanMat);
+
+		//コリジョン生成
+		auto ptrColl = AddComponent<CollisionObb>();
+		ptrColl->SetFixed(true);
+		//ptrColl->SetSleepActive(false);//ぶつからない限りスリープ状態になる
+		ptrColl->SetDrawActive(true);//コリジョンを見えるようにする
+
+		GetStage()->SetCollisionPerformanceActive(true);
+		GetStage()->SetUpdatePerformanceActive(true);
+		GetStage()->SetDrawPerformanceActive(true);
+
+		m_mapManager = GetStage()->GetSharedGameObject<MapManager>(L"MapManager");//マップマネージャーのポインタ取得
+
+	}
+
+	void Manhole::OnUpdate()
+	{
+		//m_mapManager.lock()->MapDataUpdate(m_pos, 1);//今いるセル座標はマンホールのデータということを伝える
+	}
 
 }
 //end basecross
