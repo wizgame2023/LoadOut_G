543c98c74ddab8145a9248d5ebbe2a34b4398e2f
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-10-31 12:03:00 +0900

    スプライトの中身作成

--- GameSources/Sprite.cpp
+++ GameSources/Sprite.cpp
@@ -7,7 +7,59 @@
 #include "Project.h"
 
 namespace basecross {
+	Sprite::Sprite(shared_ptr<Stage>& stagePtr,wstring textureName,Vec2 size,Vec3 pos,Vec3 rot, Col4 color) :
+		GameObject(stagePtr),
+		m_textureName(textureName),
+		m_size(size),
+		m_pos(pos),
+		m_color(color)
+	{
 
+	}
+
+	Sprite::~Sprite()
+	{
+	}
+
+	void Sprite::OnCreate()
+	{
+		// ポリゴンの自作
+		Col4 color(1, 1, 1, 1); // ポリゴンの色
+		const float w = 200.0f; // ポリゴンの幅
+		const float h = 100.0f; // ポリゴンの高さ
+		vector<VertexPositionColorTexture> m_vertices = { // 頂点データ
+			//             座標                          ,頂点色,     UV座標
+			{Vec3(-m_size.x * 0.5f, +m_size.y * 0.5f, 0), color, Vec2(0.0f, 0.0f)}, // 0
+			{Vec3(+m_size.x * 0.5f, +m_size.y * 0.5f, 0), color, Vec2(1.0f, 0.0f)}, // 1
+			{Vec3(-m_size.x * 0.5f, -m_size.y * 0.5f, 0), color, Vec2(0.0f, 1.0f)}, // 2
+			{Vec3(+m_size.x * 0.5f, -m_size.y * 0.5f, 0), color, Vec2(1.0f, 1.0f)}, // 3
+		};
+
+		vector<uint16_t> m_indices = { // 頂点インデックス（頂点のつなげ順）
+			0, 1, 2, // ←これで一つのポリゴン(三角形)
+			2, 1, 3  // ←こっちも
+		};
+
+		auto drawComp = AddComponent<PCTSpriteDraw>(m_vertices, m_indices); // スプライト用のドローコンポーネント
+		drawComp->SetTextureResource(m_textureName);//テクスチャの名前指定
+		drawComp->SetSamplerState(SamplerState::LinearWrap); // テクスチャを繰り返して貼り付ける設定
+		drawComp->SetDiffuse(Col4(1, 1, 1, 1.0f)); // ポリゴンを色を設定する
+
+		auto trans = GetComponent<Transform>();
+		trans->SetPosition(m_pos);
+		trans->SetRotation(m_rot);
+
+
+		// アルファブレンド(透過処理)を有効にする
+		SetAlphaActive(true); // true:透過を有効、false:透過を無効
+
+
+	}
+
+	void Sprite::OnUpdate()
+	{
+
+	}
 
 }
 //end basecross
