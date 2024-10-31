d414b2309c453fd59f72581c159786fcdd7a3ccc
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-10-31 10:13:14 +0900

    Updateに書いていたコードを関数に変更しました

--- GameSources/Player.cpp
+++ GameSources/Player.cpp
@@ -65,68 +65,27 @@ namespace basecross{
 
 		// インプットデバイスオブジェクト
 		auto inputDevice = App::GetApp()->GetInputDevice(); // 様々な入力デバイスを管理しているオブジェクトを取得
-
 		//コントローラーのアナログスティックの向き
-		auto& gamePad = inputDevice.GetControlerVec()[0];
+		m_controler = inputDevice.GetControlerVec()[0];
 		auto pos = GetComponent<Transform>()->GetPosition();//ポジション取得
 
 
-		//左ステックの向きにプレイヤーが進む
-		if (gamePad.bConnected)
-		{
-
-			pos.x += (gamePad.fThumbLX * 10 * Delta) * 2;
-			pos.z += (gamePad.fThumbLY * 10 * Delta) * 2;
-
-			m_Trans->SetPosition(pos);//ポジション更新
-		}
-		float deg = 0;
-		//左ステックの向きにプレイヤーも向く
-		if (gamePad.bConnected)
-		{
-			//スティックの傾きをラジアンにする
-			float rad = -atan2(gamePad.fThumbLY, gamePad.fThumbLX);
-			//ラジアンの傾きをディグリー角にする
-			deg = rad * 180 / 3.14f;
-			m_Rot.y = rad;
-			//ゲームパットの傾きが無ければ回転度は更新しない
-			if (gamePad.fThumbLY != 0.0f && gamePad.fThumbLX != 0.0f)
-			{
-				m_Trans->SetRotation(m_Rot);
-			}
-
-		}
+		PlayerMove();//プレイヤーの動き
 
 		auto mapManager = GetStage()->GetSharedGameObject<MapManager>(L"MapManager");//マップマネージャー取得
 		Vec2 selPos = mapManager->ConvertSelMap(pos);//今いるセル座標を取得
 		int selNow = mapManager->SelMapNow(pos);//現在いるセル座標に何があるかを取得
-		
 
-		//できました
-		if (m_count >= 1)//カウントが１以上なら
-		{
-			auto device = App::GetApp()->GetInputDevice().GetControlerVec();
-			if (gamePad.wPressedButtons & XINPUT_GAMEPAD_B)//Bボタンを押したとき
-			{
-				if (mapManager->SelMapNow(pos) == 1)//もし、現在いるセル座標がマンホールの上ならば
-				{		
-					m_count--;
-					mapManager->MapDataUpdate(pos, 2);//罠を設置する
+		ManholeSet(pos);//マンホールの上にわなを仕掛ける処理
 
-				}
-			}
-
-			
-		}
-
-		auto rot = GetComponent<Transform>()->GetRotation();
+		auto rot = GetComponent<Transform>()->GetRotation();//回転度を取得
 
 		//デバック用
 		wstringstream wss(L"");
 		auto scene = App::GetApp()->GetScene<Scene>();
 		//auto gameStage = scene->GetGameStage();
 		wss << L"デバッグ用文字列 "
-			<< L"\n傾き " << deg
+			<< L"\n傾き " << m_deg
 			<< L"\nPos.x " << pos.x << "\nPos.z " << pos.z
 			<<L"\nrot.x "<<rot.x << L"\nrot.y " << rot.y << "\nrot.z" << rot.z
 			<< L"\nSelPos.x " << selPos.x << "\nSelPos.y " << selPos.y
@@ -138,6 +97,62 @@ namespace basecross{
 
 		scene->SetDebugString(wss.str());
 
+	}
+
+	void Player::PlayerMove()//プレイヤーの向きや動きを管理する関数
+	{
+		auto pos = GetComponent<Transform>()->GetPosition();//ポジション取得
+		auto Delta = App::GetApp()->GetElapsedTime();
+
+		//左ステックの向きにプレイヤーが進む
+		if (m_controler.bConnected)
+		{
+
+			pos.x += (m_controler.fThumbLX * 10 * Delta) * 2;
+			pos.z += (m_controler.fThumbLY * 10 * Delta) * 2;
+
+			m_Trans->SetPosition(pos);//ポジション更新
+		}
+
+		float deg = 0;
+		//左ステックの向きにプレイヤーも向く
+		if (m_controler.bConnected)
+		{
+			//スティックの傾きをラジアンにする
+			float rad = -atan2(m_controler.fThumbLY, m_controler.fThumbLX);
+			//ラジアンの傾きをディグリー角にする
+			m_deg = rad * 180 / 3.14f;
+			m_Rot.y = rad;
+			//ゲームパットの傾きが無ければ回転度は更新しない
+			if (m_controler.fThumbLY != 0.0f && m_controler.fThumbLX != 0.0f)
+			{
+				m_Trans->SetRotation(m_Rot);
+			}
+
+		}
+
+	}
+
+	//マンホールの上にわなを仕掛ける処理
+	void Player::ManholeSet(Vec3 pos)
+	{
+		auto mapManager = GetStage()->GetSharedGameObject<MapManager>(L"MapManager");//マップマネージャー取得
+
+		if (m_count >= 1)//カウントが１以上なら
+		{
+			auto device = App::GetApp()->GetInputDevice().GetControlerVec();
+			if (m_controler.wPressedButtons & XINPUT_GAMEPAD_B)//Bボタンを押したとき
+			{
+				if (mapManager->SelMapNow(pos) == 1)//もし、現在いるセル座標がマンホールの上ならば
+				{
+					m_count--;
+					mapManager->MapDataUpdate(pos, 2);//罠を設置する
+
+				}
+			}
+
+
+		}
 	}
 
 	void Player::SetUp()
