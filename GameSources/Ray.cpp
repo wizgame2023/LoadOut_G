94aedd84e027af8b25f3d8ee90a4fff7869331ea
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-11-06 15:29:34 +0900

    レイの仮の中身作成

--- GameSources/Ray.cpp
+++ GameSources/Ray.cpp
@@ -7,7 +7,63 @@
 #include "Project.h"
 
 namespace basecross {
+	Ray::Ray(shared_ptr<Stage>& stagePtr) :
+		GameObject(stagePtr)
+	{
 
+	}
+
+	Ray::~Ray()
+	{
+	}
+
+	void Ray::OnCreate()
+	{
+		//Transform作成
+		auto ptr = GetComponent<Transform>();//Transform取得
+		ptr->SetPosition(Vec3(0.0f,0.0f,0.0f));
+		ptr->SetRotation(Vec3(0.0f,0.0f,0.0f));
+		ptr->SetScale(Vec3(10.0f,10.0f,10.0f));
+
+		//Transformに対しての等差数列
+		//Mat4x4 spanMat;
+		//spanMat.affineTransformation(
+		//	Vec3(0.0f, 0.0f, 0.0f),
+		//	Vec3(0.0f, 0.0f, 0.0f),
+		//	Vec3(0.0f, 0.0f, 0.0f),
+		//	Vec3(0.0f, 0.0f, 0.0f)
+		//);
+
+		//メッシュ生成
+		//auto ptrDraw = AddComponent<PNTStaticDraw>();
+		//ptrDraw->SetMeshResource(L"DEFAULT_CUBE");
+
+		//ptrDraw->SetMeshToTransformMatrix(spanMat);
+
+		//コリジョン生成
+		auto ptrColl = AddComponent<CollisionSphere>();
+		ptrColl->SetFixed(true);
+		ptrColl->SetSleepActive(true);//ぶつからない限りスリープ状態になる
+		ptrColl->SetDrawActive(true);//コリジョンを見えるようにする
+
+		GetStage()->SetCollisionPerformanceActive(true);
+		GetStage()->SetUpdatePerformanceActive(true);
+		GetStage()->SetDrawPerformanceActive(true);
+
+	}
+
+	void Ray::OnUpdate()
+	{
+		auto ptr = GetComponent<Transform>();//Transform取得
+		auto pos = ptr->GetPosition();
+		pos.x += 1;
+		ptr->SetPosition(pos);
+	}
+
+	void Ray::OnCollisionEnter(shared_ptr<GameObject>& other)
+	{
+		
+	}
 
 }
 //end basecross
--- GameSources/Ray.h
+++ GameSources/Ray.h
@@ -7,7 +7,19 @@
 #include "stdafx.h"
 
 namespace basecross {
+	class Ray :public GameObject
+	{
+	private:
+	public:
+		Ray(shared_ptr<Stage>& stagePtr);//コンストラクタ
+		~Ray();//デストラクタ
 
+		void OnCreate()override;//作成
+		void OnUpdate()override;//更新
+
+		void OnCollisionEnter(shared_ptr<GameObject>& other)override;
+
+	};
 
 }
 //end basecross
