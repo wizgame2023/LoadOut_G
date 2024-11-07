f5480f723cec8ec5eb763075ec19ace04ca50a17
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-11-07 11:09:13 +0900

    レイの中身作成ぶつかった時のオブジェクトを取得できるようにした

--- GameSources/Ray.cpp
+++ GameSources/Ray.cpp
@@ -2,13 +2,16 @@
 @file Ray.cpp
 @brief レイ
 */
-
+//現在の処理だと重い状態になる可能性がとても高いので修正必須だと思います
 #include "stdafx.h"
 #include "Project.h"
 
 namespace basecross {
-	Ray::Ray(shared_ptr<Stage>& stagePtr) :
-		GameObject(stagePtr)
+	Ray::Ray(shared_ptr<Stage>& stagePtr,Vec3 pos,float angle, GameObject& parentObj) :
+		GameObject(stagePtr),
+		m_pos(pos),
+		m_rad(angle),
+		m_parentObj(parentObj)
 	{
 
 	}
@@ -21,7 +24,7 @@ namespace basecross {
 	{
 		//Transform作成
 		auto ptr = GetComponent<Transform>();//Transform取得
-		ptr->SetPosition(Vec3(0.0f,0.0f,0.0f));
+		ptr->SetPosition(m_pos);
 		ptr->SetRotation(Vec3(0.0f,0.0f,0.0f));
 		ptr->SetScale(Vec3(10.0f,10.0f,10.0f));
 
@@ -54,15 +57,33 @@ namespace basecross {
 
 	void Ray::OnUpdate()
 	{
+		auto delta = App::GetApp()->GetElapsedTime();//デルタタイム取得
 		auto ptr = GetComponent<Transform>();//Transform取得
 		auto pos = ptr->GetPosition();
-		pos.x += 1;
+		float speed = 1;
+		pos.x += cos(m_rad) * speed * delta;
+		pos.z += sin(m_rad) * speed * delta;
 		ptr->SetPosition(pos);
 	}
 
 	void Ray::OnCollisionEnter(shared_ptr<GameObject>& other)
 	{
-		
+		//ぶつかったオブジェクトが今までぶつかったことのないオブジェクトなら配列に入れる
+		for (auto a : m_discoveryObj)
+		{
+			if (a.lock() != other)//オブジェクトが前覚えている物でないとき
+			{
+				m_discoveryObj.push_back(other);//記憶する
+			}
+			GetDisObj();//取得したオブジェクトを渡す
+		}
+		GetStage()->RemoveGameObject<Ray>(GetThis<Ray>());//自分自身を消す
+	}
+
+	//取得したオブジェクトを渡す
+	vector<weak_ptr<GameObject>> Ray::GetDisObj()
+	{
+		return m_discoveryObj;
 	}
 
 }
--- GameSources/Ray.h
+++ GameSources/Ray.h
@@ -1,6 +1,6 @@
 /*!
 @file Ray.h
-@brief レイ
+@brief レイ　視界
 */
 
 #pragma once
@@ -10,14 +10,25 @@ namespace basecross {
 	class Ray :public GameObject
 	{
 	private:
+		static vector<weak_ptr<GameObject>> m_discoveryObj;//ぶつかったオブジェクトのアドレスを取る 静的変数
+
+		float m_range;//視界の射程
+		float m_move;//元居た地点からどれくらい移動したか測る変数
+		GameObject& m_parentObj;//親オブジェクト
+
+		Vec3 m_pos;//位置
+		float m_rad;//角度
+
 	public:
-		Ray(shared_ptr<Stage>& stagePtr);//コンストラクタ
+		Ray(shared_ptr<Stage>& stagePtr, Vec3 pos, float angle,GameObject& parentObj);//コンストラクタ
 		~Ray();//デストラクタ
 
 		void OnCreate()override;//作成
 		void OnUpdate()override;//更新
 
 		void OnCollisionEnter(shared_ptr<GameObject>& other)override;
+
+		vector<weak_ptr<GameObject>> GetDisObj();//取得したオブジェクトを渡す
 
 	};
 
