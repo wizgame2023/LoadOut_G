7f6d0bc8d729060ae54ac6d03a86040614f38997
UniverseSushi3319 <160200306+UniverseSushi3319@users.noreply.github.com>
2024-11-08 16:06:04 +0900

    土台部分を追加

--- GameSources/Sky_base.cpp
+++ GameSources/Sky_base.cpp
@@ -7,7 +7,22 @@
 #include "Project.h"
 
 namespace basecross {
+	void Sky_base::OnCreate()
+	{
+		std::vector<VertexPositionTexture> vertices = {
+			{Vec3(-0.5f,+0.5f,0.0f),Vec2(0.0f,0.0f)},
+			{Vec3(+0.5f,+0.5f,0.0f),Vec2(1.0f,0.0f)},
+			{Vec3(-0.5f,-0.5f,0.0f),Vec2(0.0f,1.0f)},
+			{Vec3(+0.5f,-0.5f,0.0f),Vec2(1.0f,1.0f)}
+		};
 
+		std::vector<uint16_t> indices = {
+			0, 1, 2, 2, 1, 3
+		};
 
+		auto drawComp = AddComponent<PTStaticDraw>();
+		drawComp->SetOriginalMeshUse(true);
+		drawComp->CreateOriginalMesh(vertices, indices);
+	}
 }
 //end basecross
--- GameSources/Sky_base.h
+++ GameSources/Sky_base.h
@@ -7,7 +7,15 @@
 #include "stdafx.h"
 
 namespace basecross {
+	class Sky_base : public GameObject
+	{
+	public:
+		Sky_base(const std::shared_ptr<Stage>& stage)
+			: GameObject(stage)
+		{
+		}
 
-
+		void OnCreate() override;
+	};
 }
 //end basecross
