cee4a3e566815305d895976938ebf624418c05d0
UniverseSushi3319 <160200306+UniverseSushi3319@users.noreply.github.com>
2024-11-12 09:35:27 +0900

    スカイブロックの製作

--- GameSources/happysky.cpp
+++ GameSources/happysky.cpp
@@ -5,9 +5,29 @@
 
 #include "stdafx.h"
 #include "Project.h"
+#include <string>
 
 namespace basecross {
+	const std::map<std::wstring, Vec3> happysky::pairs = {
+		{ L"000_000_000", Vec3(0  ,0,  +0.5f) },
+		{ L"000_090_000", Vec3(+0.5f, 0, 0  ) },
+		{ L"000_180_000", Vec3(0  ,0,  -0.5f) },
+		{ L"000_270_000", Vec3(-0.5f, 0, 0  ) },
+		{ L"090_000_000", Vec3(0  ,-0.5f,0  ) },
+		{ L"270_000_000", Vec3(0  ,-0.5f,0  ) }
+	};
+	
+	void happysky::OnCreate()
+	{
+		for (const auto& pair : pairs)
+		{
+			auto sky_base = ObjectFactory::Create<Sky_base>(GetStage());
 
-
+			auto planeDrawComp = sky_base->GetComponent<PTStaticDraw>();
+			planeDrawComp->SetTextureResource(pair.first);
+			planeDrawComp->SetDepthStencilState(DepthStencilState::None);
+			planeDrawComp->SetSamplerState(SamplerState::LinearClamp);
+		}
+	}
 }
 //end basecross
--- GameSources/happysky.h
+++ GameSources/happysky.h
@@ -9,7 +9,21 @@
 namespace basecross {
 	class happysky : public GameObject
 	{
+	public : 
+		static const std::map<std::wstring, Vec3> pairs;
+
+	private :
+		std::vector<std::shared_ptr<GameObject>> m_skybase;
+
 	public :
+		happysky(const shared_ptr<Stage>& stage)
+			: GameObject(stage)
+		{
+		}
+
+		void OnCreate() override;
+		void OnUpdate() override;
+		void OnDraw() override;
 	};
 }
 //end basecross
