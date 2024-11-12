4174d05f3839c372b4217ab64734b60584ee56e8
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-11-12 13:41:42 +0900

    数字用のスプライトファイル作成

---
+++ GameSources/SpriteNum.cpp
@@ -0,0 +1,74 @@
+/*!
+@file SpriteNum.cpp
+@brief 数字を出すスプライト 作成途中
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+	SpriteNum::SpriteNum(shared_ptr<Stage>& stagePtr, wstring textureName, Vec2 size,int num,Vec3 pos, Vec3 rot):
+		GameObject(stagePtr),
+		m_textureName(textureName),
+		m_size(size),
+		m_pos(pos),
+		m_rot(rot),
+		m_num(num)
+	{
+
+	}
+
+	SpriteNum::~SpriteNum()
+	{
+
+	}
+
+	void SpriteNum::OnCreate()
+	{
+		// ポリゴンの自作
+		Col4 color(1, 1, 1, 1); // ポリゴンの色
+		const float w = 200.0f; // ポリゴンの幅
+		const float h = 100.0f; // ポリゴンの高さ
+		vector<VertexPositionColorTexture> m_vertices = { // 頂点データ
+
+			//             座標                          ,頂点色,     UV座標
+			{Vec3(-m_size.x * 0.5f, +m_size.y * 0.5f, 0), color, Vec2(0.1f*(m_num-1), 0.0f)}, // 0
+			{Vec3(+m_size.x * 0.5f, +m_size.y * 0.5f, 0), color, Vec2(0.1f*m_num, 0.0f)}, // 1
+			{Vec3(-m_size.x * 0.5f, -m_size.y * 0.5f, 0), color, Vec2(0.1f*(m_num-1), 1.0f)}, // 2
+			{Vec3(+m_size.x * 0.5f, -m_size.y * 0.5f, 0), color, Vec2(0.1f*m_num, 1.0f)}, // 3
+		};
+
+		vector<uint16_t> m_indices = { // 頂点インデックス（頂点のつなげ順）
+			0, 1, 2, // ←これで一つのポリゴン(三角形)
+			2, 1, 3  // ←こっちも
+		};
+
+		auto drawComp = AddComponent<PCTSpriteDraw>(m_vertices, m_indices); // スプライト用のドローコンポーネント
+		drawComp->SetTextureResource(m_textureName);//テクスチャの名前指定
+		drawComp->SetSamplerState(SamplerState::LinearWrap); // テクスチャを繰り返して貼り付ける設定
+		drawComp->SetDiffuse(Col4(1, 1, 1, 1.0f)); // ポリゴンを色を設定する
+
+		auto trans = GetComponent<Transform>();
+		trans->SetPosition(m_pos);
+		trans->SetRotation(m_rot);
+
+
+		// アルファブレンド(透過処理)を有効にする
+		SetAlphaActive(true); // true:透過を有効、false:透過を無効
+
+		//m_vertices[0].textureCoordinate = Vec2(0.0f, 0.0f);
+
+		////時間経過で数値が変わるようにする
+		//for (auto num : m_vertices)
+		//{
+
+		//}
+
+	}
+
+	void SpriteNum::OnUpdate()
+	{
+	}
+
+}
+//end basecross
---
+++ GameSources/SpriteNum.h
@@ -0,0 +1,45 @@
+/*!
+@file Sprite.h
+@brief スプライト用のクラス
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class SpriteNum :public GameObject
+	{
+	private:
+		//画像の表示サイズ
+		float m_width;
+		float m_heigth;
+		int m_layer;
+		Vec2 m_size;//画像の大きさ
+
+		int m_num;//表示する数値
+
+
+		//vector<VertexPositionColorTexture> m_vertices;
+		//vector<uint16_t> m_indices;
+		//shared_ptr<PCTSpriteDraw> m_draw;
+		wstring m_textureName;//テクスチャの名前
+
+		shared_ptr<Transform> m_trans;
+
+		Vec3 m_pos;
+		Vec3 m_rot;
+		Col4 m_color;
+
+
+	public:
+		SpriteNum(shared_ptr<Stage>& stagePtr, wstring textureName, Vec2 size,int num,Vec3 pos=Vec3(0.0f,0.0f,0.0f), Vec3 rot=Vec3(0.0f,0.0f,0.0f));//コンストラクタ
+		~SpriteNum();//デストラクタ
+
+		void OnCreate()override;//作成
+		void OnUpdate()override;//更新
+
+	};
+
+
+}
+//end basecross
