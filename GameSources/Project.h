5d179d3264020babf1dbb9fdec7ea8aefccce5c5
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-11-15 13:29:39 +0900

    インクルード追加

--- GameSources/Project.h
+++ GameSources/Project.h
@@ -43,6 +43,7 @@
 #include "SpriteNum.h"//数字のスプライト
 #include "WaterPillar.h"//水柱
 #include "MiniMapPlayer.h"//ミニマップのPlayer
+#include "MiniMapItem.h"//ミニマップのItem
 
 //マネージャー用
 #include "MiniMapManager.h"//ミニマップ生成
