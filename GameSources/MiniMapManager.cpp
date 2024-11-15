314d73c513ab76ca3e83b0611067a067d4ea65ab
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-11-15 09:22:22 +0900

    新しい関数追加、座標変更など

--- GameSources/MiniMapManager.cpp
+++ GameSources/MiniMapManager.cpp
@@ -22,13 +22,30 @@ namespace basecross {
 	{
 		auto stage = GetStage();//ステージ取得
 		float Lenght = 225.0f;//ミニマップの直径
-		stage->AddGameObject<Sprite>(L"Gray", Vec2(Lenght, Lenght), Vec3(-640+(Lenght/2.0f)+10.0f, 400.0f - (Lenght / 2.0f)-10.0f, 0.0f));
+		stage->AddGameObject<Sprite>(L"Gray", Vec2(Lenght, Lenght), Vec3(-640+(Lenght/2.0f), 400.0f - (Lenght / 2.0f), 0.0f));
+
+		m_startPos = Vec3(-640 + (Lenght / 2.0f), 400.0f - (Lenght / 2.0f),5.0f);//スタートポジション
+		auto test = m_startPos;
+		auto a = 0;
 	}
 
 	void MiniMapManager::OnUpdate()
 	{
 
 	}
+
+	//ミニマップ用の壁を生成
+	void MiniMapManager::CreateWall()
+	{
+
+	}
+
+	Vec3 MiniMapManager::GetStartPos()
+	{
+		return m_startPos;
+	}
+
+	
 
 }
 //end basecross
--- GameSources/MiniMapManager.h
+++ GameSources/MiniMapManager.h
@@ -10,12 +10,17 @@ namespace basecross {
 	class MiniMapManager :public GameObject
 	{
 	private:
+		Vec3 m_startPos;//初期位置(ピポット)
 	public:
 		MiniMapManager(shared_ptr<Stage>& stage);
 		~MiniMapManager();
 
 		void OnCreate()override;//作成
 		void OnUpdate()override;//更新
+
+		void CreateWall();//ミニマップの壁を生成する
+
+		Vec3 GetStartPos();//ミニマップの原点を渡す
 	};
 
 }
