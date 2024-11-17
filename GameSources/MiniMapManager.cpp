51ceb70445a0ef161c4e7f298c54417df6ce8b14
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-11-17 14:25:17 +0900

    ミニマップに敵が出る関数追加

--- GameSources/MiniMapManager.cpp
+++ GameSources/MiniMapManager.cpp
@@ -20,17 +20,18 @@ namespace basecross {
 
 	void MiniMapManager::OnCreate()
 	{
-		auto stage = GetStage();//ステージ取得
-		float Lenght = 225.0f;//ミニマップの直径
-		stage->AddGameObject<Sprite>(L"Gray", Vec2(Lenght, Lenght), Vec3(-640+(Lenght/2.0f), 400.0f - (Lenght / 2.0f), 0.0f));
+		//auto stage = GetStage();//ステージ取得
+		//float Lenght = 225.0f;//ミニマップの直径
+		//stage->AddGameObject<Sprite>(L"Gray", Vec2(Lenght, Lenght), Vec3(-640+(Lenght/2.0f), 400.0f - (Lenght / 2.0f), 0.0f));
 
-		m_startPos = Vec3(-640 + (Lenght / 2.0f), 400.0f - (Lenght / 2.0f),5.0f);//スタートポジション
-		auto test = m_startPos;
-		auto a = 0;
-		m_mapSize = 200;//mapの直径
-		m_mapMagnification = Lenght / m_mapSize;//マップの倍率
+		//m_startPos = Vec3(-640 + (Lenght / 2.0f), 400.0f - (Lenght / 2.0f),5.0f);//スタートポジション
+		//auto test = m_startPos;
+		//auto a = 0;
+		//m_mapSize = 200;//mapの直径
+		//m_mapMagnification = Lenght / m_mapSize;//マップの倍率
 
-		CreateManhole();//マンホールを生成
+		//CreateManhole();//マンホールを生成//もしかしたらエラーでる？
+		//CreateEnemy();
 	}
 
 	void MiniMapManager::OnUpdate()
@@ -84,6 +85,31 @@ namespace basecross {
 
 				stage->AddGameObject<Sprite>(L"Red", Vec2((itemScale.x * m_mapMagnification), (itemScale.z * m_mapMagnification)),
 					Vec3(m_startPos.x + (itemPos.x * m_mapMagnification), m_startPos.y + (itemPos.z * m_mapMagnification), 0.0f), Vec3(0.0f, 0.0f, 0.0f),Col4(1.0f,1.0f,1.0f,1.0f), 10);
+			}
+		}
+
+	}
+
+	//ミニマップ用の敵を作成
+	void MiniMapManager::CreateEnemy()
+	{
+		auto stage = GetStage();//ステージ取得
+		//ステージのオブジェクトを全て取得
+		auto obj = stage->GetGameObjectVec();
+		//取得したオブジェクトがアイテムに変換できたら配列に入れる
+		for (auto manhole : obj)
+		{
+			if (dynamic_pointer_cast<Enemy>(manhole))//アイテム型にキャストする
+			{
+				float Lenght = 225.0f;//ミニマップの直径
+
+				auto castEnemy = dynamic_pointer_cast<Actor>(manhole);
+				auto itemTrans = manhole->GetComponent<Transform>();
+				auto itemPos = itemTrans->GetPosition();
+				auto itemScale = itemTrans->GetScale();
+
+				stage->AddGameObject<MiniMapActor>(castEnemy,L"MiniEnemy", Vec2((itemScale.x * m_mapMagnification)*10, (itemScale.z * m_mapMagnification)*10),
+					Vec3(m_startPos.x + (itemPos.x * m_mapMagnification), m_startPos.y + (itemPos.z * m_mapMagnification), 6.0f),m_mapSize,Lenght, Vec3(0.0f, 0.0f, 0.0f), Col4(1.0f, 1.0f, 1.0f, 1.0f));
 			}
 		}
 
