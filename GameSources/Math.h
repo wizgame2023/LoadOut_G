4231ebe256f35f5519747aee161d75691b63e80b
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-11-22 11:33:15 +0900

    Mathにゲッター追加

--- GameSources/Math.h
+++ GameSources/Math.h
@@ -8,6 +8,26 @@
 
 namespace basecross {
 
+	static class Math
+	{
+	public:
+		float GetDistance(Vec3 a, Vec3 b)
+		{
+			float numX = a.x - b.x;
+			float numY = a.y - b.y;
+			float numZ = a.z - b.z;
 
+			return  sqrtf(numX * numX + numY * numY + numZ * numZ);
+		}
+
+		float GetCircleRange(float r, Vec3 a, Vec3 b)
+		{
+			float numX = a.x - b.x;
+			float numY = a.y - b.y;
+			float numZ = a.z - b.z;
+
+			return numX * numX + numY * numY + numZ * numZ <= r * r;
+		}
+	};
 }
 //end basecross
