7b0a0fd46c6fa89afeec4c8f633ffe29c435b308
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-11-22 15:24:34 +0900

    巡回ステートのコメント追加

--- GameSources/Patrol.h
+++ GameSources/Patrol.h
@@ -12,31 +12,38 @@ namespace basecross {
 	class Patrol :public StateBase
 	{
 	private:
+		//Vec3型のメンバー変数
 		Vec3 m_ownerPos;
 		Vec3 m_ownerRot;
 		Vec3 m_destinationPos;
 		Vec3 m_right;
 		Vec3 m_forward;
 
+		//int型メンバー変数
 		int m_rnd;
 		int m_numbers;
 
+		//float型メンバー変数
 		float m_time;
 		float m_moveTime;
 		float m_distance;
 		const float m_point;
 
+		//bool型メンバー変数
 		bool m_destinationDecision;
 		bool m_wallCheck;
 		bool m_rightCheck;
 		bool m_forwardCheck;
 		bool m_minus;
 
+		//shared_ptr型メンバー変数
 		shared_ptr<Transform> m_trans;
 
+		//weak_ptr型メンバー変数
 		weak_ptr<Ray> m_forwardRay;
 		weak_ptr<Ray>m_playerRay;
 	public:
+		//コンストラクタの宣言
 		Patrol(const shared_ptr<Enemy> ptrOwner) :
 			StateBase(ptrOwner),
 			m_ownerPos(0, 0, 0),
