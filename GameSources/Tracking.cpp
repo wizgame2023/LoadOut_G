a74413607006f9f8b38ac5aa61d731e89c4871dd
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-11-22 12:56:52 +0900

    追跡処理変更

--- GameSources/Tracking.cpp
+++ GameSources/Tracking.cpp
@@ -19,12 +19,12 @@ namespace basecross {
 	{
 		//playerを追いかける処理
 		auto app = App::GetApp;
+		wstringstream wss(L"");
 		m_trans = m_Owner->GetComponent<Transform>();//所有者(Enemy)のTransformを取得
 		m_ownerPos = m_trans->GetPosition();//所有者(Enemy)のポジションを取得
 		auto player = app()->GetScene<Scene>()->GetActiveStage()->GetSharedGameObject<Player>(L"Player");//playerを取得
 		m_playerPos = player->GetComponent<Transform>()->GetPosition();//playerのポジションを取得
 		m_playerRay = m_Owner->GetPlayerRay();
-		m_time += app()->GetElapsedTime();
 		float rad = atan2f((m_ownerPos.x - m_playerPos.x), (m_ownerPos.z - m_playerPos.z));//所有者(Enemy)を中心にplayerの方向を計算
 		m_ownerRot.y = rad;//playerの方向に向く
 		float deg = rad * 180 / XM_PI;//ラジアンをディグリーに変換（デバック用）
@@ -32,32 +32,38 @@ namespace basecross {
 		m_ownerPos.z+=-cos(rad) * m_Owner->GetSpeed() * app()->GetElapsedTime();
 		m_ownerPos.y = 2.5f;
 	
-		m_trans->SetRotation(m_ownerRot);//所有者(Enemy)のローテーションの更新
-		m_trans->SetPosition(m_ownerPos);//所有者(Enemy)のポジションの更新
-
-
-		if (m_playerRay.lock()->GetDisObj().size() > 0)
+		Math math;
+		auto cr= math.GetCircleRange(60, m_ownerPos, m_playerPos);
+		auto x = (m_ownerPos.x - m_playerPos.x) * (m_ownerPos.x - m_playerPos.x);
+		auto y = (m_ownerPos.y - m_playerPos.y) * (m_ownerPos.y - m_playerPos.y);
+		auto z = (m_ownerPos.z - m_playerPos.z) * (m_ownerPos.z - m_playerPos.z);
+		if (sqrtf(x + y + z) <= 60)
 		{
-			for (auto obj : m_playerRay.lock()->GetDisObj())
-			{
-				if (obj.lock()->FindTag(L"Wall") && m_time > 3)
-				{
-					m_Owner->ChangeState<Patrol>();
-				}
-				if (obj.lock()->FindTag(L"Player"))
-				{
-					m_time = 0;
-				}
-			}
+			m_time = 0;
 		}
+		else
+		{
+			m_time += app()->GetElapsedTime();
+		}
+
+
+		if (m_time >= 3)
+		{
+			m_Owner->ChangeState<Patrol>();
+		}
+
 		if (m_Owner->GetDistance(m_ownerPos, m_playerPos) < 7)
 		{
 			m_Owner->ChangeState<Attack>();
 		}
-		auto d=m_Owner->GetDistance(m_ownerPos, m_playerPos);
+		 auto a = cr;
+
+
+		m_trans->SetRotation(m_ownerRot);//所有者(Enemy)のローテーションの更新
+		m_trans->SetPosition(m_ownerPos);//所有者(Enemy)のポジションの更新
+		m_Owner->SetAngle(rad + XM_PI * 0.5f);
 
 		//デバックログ
-		wstringstream wss(L"");
 		auto scene = App::GetApp()->GetScene<Scene>();
 		wss << L"プレイヤーPos.x : " << m_playerPos.x
 			<< L"\nプレイヤーPos.z : " << m_playerPos.z
@@ -65,7 +71,7 @@ namespace basecross {
 			<< L"\n敵の回転（deg）" << deg
 			<< L"\n敵のPos.x : " << m_ownerPos.x
 			<< L"\n敵のPos.z : " << m_ownerPos.z
-			<< L"\nd :  " << d
+			<< L"\na : " << a
 			<< endl;
 		scene->SetDebugString(wss.str());
 	}
