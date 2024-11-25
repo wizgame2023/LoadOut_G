8dfb2eab029d75f4d68f0b1a492edd8f47f7ab36
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-11-25 14:37:41 +0900

    基本的なものだけ作成

--- GameSources/StageManager.cpp
+++ GameSources/StageManager.cpp
@@ -7,6 +7,26 @@
 #include "Project.h"
 
 namespace basecross {
+	StageManager::StageManager(shared_ptr<Stage>& stagePtr) :
+		GameObject(stagePtr)
+	{
+
+	}
+
+	StageManager::~StageManager()
+	{
+
+	}
+
+	void StageManager::OnCreate()
+	{
+
+	}
+
+	void StageManager::OnUpdate()
+	{
+
+	}
 
 
 }
--- GameSources/StageManager.h
+++ GameSources/StageManager.h
@@ -7,7 +7,17 @@
 #include "stdafx.h"
 
 namespace basecross {
+	class StageManager :public GameObject
+	{
+	private:
 
+	public:
+		StageManager(shared_ptr<Stage>& stagePtr);
+		~StageManager();
+
+		void OnCreate()override;
+		void OnUpdate()override;
+	};
 
 }
 //end basecross
