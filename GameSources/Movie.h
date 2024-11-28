ea827878ae940b6f196ed478ccc89bbb6494bef9
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-11-28 11:11:19 +0900

    ムービー用の親クラス中身作成

--- GameSources/Movie.h
+++ GameSources/Movie.h
@@ -5,12 +5,15 @@
 
 #pragma once
 #include "stdafx.h"
+#include "MyCamera.h"
 
 namespace basecross {
 	class Movie :public GameObject
 	{
 	private:
-
+		weak_ptr<MyCamera> m_stageCamera;//ステージ用のカメラ
+		shared_ptr<Camera> m_movieCamera;//ムービー用のかめら
+		Vec3 testPos;
 	public:
 		Movie(shared_ptr<Stage>& stagePtr);
 		~Movie();
@@ -20,6 +23,9 @@ namespace basecross {
 
 		//ムービー用のカメラに変更させる
 		virtual void CameraChange();
+
+		//カメラを目的地に向けて移動させる
+		virtual Vec3 CameraMove(float speed,Vec3 pos,Vec3 tagetPos);
 	};
 
 }
