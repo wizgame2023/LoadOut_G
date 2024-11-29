ba67ce3d68fbddb910ee4730b3058b34d76faf9c
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-11-29 16:02:58 +0900

    ゲームクリアのムービ作成

--- GameSources/MovieGameClear.cpp
+++ GameSources/MovieGameClear.cpp
@@ -19,11 +19,50 @@ namespace basecross {
 
 	void MovieGameClear::OnCreate()
 	{
+		CameraChange();//カメラ変更
+		m_tagetPos = m_movieCamera->GetEye() - Vec3(0.0f, 15.0f, 0.0f);//目的地
 
 	}
 
 	void MovieGameClear::OnUpdate()
 	{
+		auto stage = GetStage();
+		auto delta = App::GetApp()->GetElapsedTime();//デルタタイム
+		auto cameraPos = m_movieCamera->GetEye();
+		auto player = stage->GetSharedGameObject<Player>(L"Player");//プレイヤー取得
+		auto playerTrans = player->GetComponent<Transform>();
+		auto playerPos = playerTrans->GetPosition();
+
+		player->MoveSwich(false);//playerを動かさない
+		Vec3 moveVec= Vec3(cameraPos.x - playerPos.x, cameraPos.y - playerPos.y, cameraPos.z - playerPos.z);//カメラとPlayerの距離ベクトル
+		float angle = atan2(moveVec.z, moveVec.x);//角度を取得
+		playerTrans->SetRotation(Vec3(0.0f, -angle, 0.0f));//カメラ目線になるようにする
+		
+		if (m_count == 0)//処理①
+		{
+			if (cameraPos != m_tagetPos)
+			{
+				cameraPos.y -= 10.0f*delta;//間接的に移動
+				m_movieCamera->SetEye(cameraPos);
+				if (abs(cameraPos.y - m_tagetPos.y) <= 0.5f)//ほぼPosがターゲットののPosと一緒なら
+				{
+					cameraPos = m_tagetPos;
+					m_movieCamera->SetEye(cameraPos);
+					m_count = 1;//更新
+				}
+				m_movieCamera->SetEye(cameraPos);
+
+			}
+
+		}
+		if (m_count == 1)//処理②
+		{
+			//ステージマネージャーにゲームクリアのフラグを渡す
+			auto stageManager = stage->GetSharedGameObject<StageManager>(L"StageManager");
+			stageManager->SetClearFlag(true);
+
+		}
+
 
 	}
 
