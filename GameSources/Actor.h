9b70795afa1ccedb10d4d56cfe5bff786e4dfdcd
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-11-29 14:56:11 +0900

    動くか動かないかの処理追加

--- GameSources/Actor.h
+++ GameSources/Actor.h
@@ -12,12 +12,17 @@ namespace basecross {
 	private:
 		float m_angle;//角度
 
+	protected:
+		bool m_move;//動くか動かないかのフラグ
+
 	public:
 		Actor(shared_ptr<Stage>& stagePtr);
 		~Actor();
 
 		virtual float GetAngle();
 		virtual void SetAngle(float angle);
+
+		void MoveSwich(bool onOff);//動いていいかのフラグ
 
 		//void OnCreate()override;
 		//void OnUpdate()override;
