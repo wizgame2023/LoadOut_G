bcebf519bcedc57fbc8a210a4e3586cbf787a62b
UniverseSushi3319 <160200306+UniverseSushi3319@users.noreply.github.com>
2024-12-03 11:32:12 +0900

    Stage2の原型を作成

--- GameSources/GameStage.cpp
+++ GameSources/GameStage.cpp
@@ -16,7 +16,7 @@ namespace basecross {
 		const Vec3 at(0.0f);
 		auto PtrView = CreateView<SingleView>();
 		//ƒrƒ…[‚ÌƒJƒƒ‰‚Ìİ’è
-		auto PtrCamera = ObjectFactory::Create<MyCamera>(Vec3(0.0f, 50.0f, -30.0f));
+		auto PtrCamera = ObjectFactory::Create<MyCamera>(Vec3(0.0f, 50.0f, -3 0.0f));
 		PtrView->SetCamera(PtrCamera);
 		PtrCamera->SetEye(eye);
 		PtrCamera->SetAt(at);
