9cbaabac2c7555c3e25de73dcee958fa7d56cc28
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-12-12 22:02:13 +0900

    メッシュ更新

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -155,7 +155,7 @@ namespace basecross{
 		//ボーンモデル
 		auto boneModelMesh = MeshResource::CreateBoneModelMesh(modPath, L"police_ver1.1.bmf");//敵のメッシュ
 		app->RegisterResource(L"Boss_Mesh_Kari", boneModelMesh);
-		boneModelMesh = MeshResource::CreateBoneModelMesh(modPath, L"kid_ver1.1.bmf");//プレイヤーのメッシュ
+		boneModelMesh = MeshResource::CreateBoneModelMesh(modPath, L"kid_ver1.2.bmf");//プレイヤーのメッシュ
 		app->RegisterResource(L"kid_Mesh", boneModelMesh);
 		boneModelMesh = MeshResource::CreateBoneModelMesh(modPath, L"battry_ver1.2.bmf");//電池用のメッシュ
 		app->RegisterResource(L"Battry", boneModelMesh);
