1d010768ee94d1ca92090ac811c76e90a559aca1
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-12-13 12:50:27 +0900

    柱上のエフェクト作成

--- GameSources/PillarEfect.h
+++ GameSources/PillarEfect.h
@@ -7,7 +7,43 @@
 #include "stdafx.h"
 
 namespace basecross {
+	class PillarEffect : public GameObject
+	{
+	private:
+		std::vector<VertexPositionColorTexture> m_vertices;
+		std::vector<uint16_t> m_indices;
 
+		std::shared_ptr<BcPCTStaticDraw> m_drawComp;
+
+		Vec3 m_pos;//ポジション
+		float m_height;//高さ
+		float m_topRadius;//上の円の大きさ
+		float m_bottomRadius;//下の円の大きさ
+		int m_square;//角数
+		Vec2 m_loops;//画像をどうループさせるか
+		Col4 m_topColor;//上の頂点の色
+		Col4 m_bottomColor;//下の頂点の色
+		wstring m_textureName;
+		Vec2 m_scrollVelocity;//XY方向の速度
+
+		bool m_isUpdate = false;
+
+		Vec2 m_test;
+
+	protected:
+		void InitializeVertices();
+
+	public:
+		PillarEffect(const shared_ptr<Stage>& stagePtr,Vec3 pos, wstring textureName,Vec2 velovity, int square = 36);
+		~PillarEffect();
+
+		void OnCreate() override;
+		void OnUpdate() override;
+
+		void SetScrollPerSecond(Vec2 velocity);
+		void UpdateTest(bool OnOff);
+
+	};
 
 }
 //end basecross
