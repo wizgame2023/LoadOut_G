9f5de266ee96b0e715c7c5fc65450f41f11b234f
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-12-17 18:33:58 +0900

    生成クラスを変更

--- GameSources/MiniMapManager.cpp
+++ GameSources/MiniMapManager.cpp
@@ -135,7 +135,7 @@ namespace basecross {
 				auto itemPos = itemTrans->GetPosition();
 				auto itemScale = itemTrans->GetScale();
 
-				stage->AddGameObject<Sprite>(L"Bule", Vec2((itemScale.x * m_mapMagnification), (itemScale.z * m_mapMagnification)),
+				stage->AddGameObject<ManholeSprite>(castManhole, L"Bule",Vec2((itemScale.x * m_mapMagnification), (itemScale.z * m_mapMagnification)),
 					Vec3(m_startPos.x + (itemPos.x * m_mapMagnification), m_startPos.y + (itemPos.z * m_mapMagnification), 0.0f), Vec3(0.0f, 0.0f, 0.0f),Col4(1.0f,1.0f,1.0f,1.0f), 10);
 			}
 		}
