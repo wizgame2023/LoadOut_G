aa9b2f77941d653c5a5204761057c1fbcb5cae45
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-12-17 14:23:03 +0900

    ファイルへの追加

---
+++ GameSources/NumberManager.cpp
@@ -0,0 +1,18 @@
+/*!
+@file Character.cpp
+@brief キャラクターなど実体
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross{
+
+	//初期化
+	void Foo::OnCreate()
+	{
+		//test
+	}
+
+}
+//end basecross
