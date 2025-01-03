f691eec9a2d7cc6d81c83eca7491873beb604a77
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-01-03 14:16:52 +0900

    予定しているゲームステージを作成

---
+++ GameSources/GameStage19.h
@@ -0,0 +1,45 @@
+/*!
+@file GameStage19.h
+@brief ゲームステージ
+*/
+
+#pragma once
+#include "stdafx.h"
+#include "MiniMapManager.h"
+
+namespace basecross {
+
+	//--------------------------------------------------------------------------------------
+	//	ゲームステージクラス
+	//--------------------------------------------------------------------------------------
+	class GameStage19 : public Stage {
+		//ビューの作成
+		void CreateViewLight();
+		shared_ptr<SoundItem> m_BGM;
+		shared_ptr<XAudio2Manager> m_bgmManager;
+		shared_ptr<MiniMapManager> m_miniMapManager;
+
+	public:
+		//構築と破棄
+		GameStage19() :Stage() {}
+		virtual ~GameStage19() {}
+		//初期化
+		virtual void OnCreate()override;
+		virtual void OnUpdate()override;
+		void OnDestroy()override;
+		void OutWallCreate(int selLength);//外壁を作る関数
+		int GameEnemyState();//全ての敵の遷移状態を受け取る
+		int EnemyNow = 0;
+
+		void CreatePlayer();//Player生成
+		void CreateBattery();//アイテム生成
+		void CreateEnemy();//Enemy生成
+
+		void SetEnemy(int num);
+
+	};
+
+
+}
+//end basecross
+
