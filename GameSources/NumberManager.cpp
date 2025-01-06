46a279d8e2e3893f0a6f279de0f2aca1e81a80f4
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-01-06 12:42:03 +0900

    動的に数値が動くようにした、桁数を自由に決めれるようにした

--- GameSources/NumberManager.cpp
+++ GameSources/NumberManager.cpp
@@ -7,11 +7,27 @@
 #include "Project.h"
 
 namespace basecross {
-	NuberManager::NuberManager(shared_ptr<Stage>& stage, int nuber, Vec2 scale, Vec3 pos) :
+	NuberManager::NuberManager(shared_ptr<Stage>& stage, int number, Vec2 scale, Vec3 pos) :
 		GameObject(stage),
-		m_nuber(nuber),
+		m_number(number),
 		m_scale(scale),
 		m_pos(pos)
+	{
+		if (m_number < 10)
+		{
+			m_numLength = false;
+		}
+		if (m_number >= 10)
+		{
+			m_numLength = true;
+		}
+	}
+	NuberManager::NuberManager(shared_ptr<Stage>& stage, int number, Vec2 scale, Vec3 pos,bool numLenght) :
+		GameObject(stage),
+		m_number(number),
+		m_scale(scale),
+		m_pos(pos),
+		m_numLength(numLenght)
 	{
 	}
 	NuberManager::~NuberManager()
@@ -20,19 +36,33 @@ namespace basecross {
 
 	void NuberManager::OnCreate()
 	{
-		if (m_nuber < 10)
+		if (!m_numLength)
 		{
-			m_sprite = GetStage()->AddGameObject<SpriteNum>(L"Number", m_scale, m_nuber, m_pos);
+			m_spriteOne = GetStage()->AddGameObject<SpriteNum>(L"Number", m_scale, m_number, m_pos);
 		}
-		else if (m_nuber >= 10)
+		if (m_numLength)
 		{
-			m_sprite = GetStage()->AddGameObject<SpriteNum>(L"Number", m_scale , m_nuber / 10, Vec3(m_pos.x - 20, m_pos.y, m_pos.z));
-			m_sprite = GetStage()->AddGameObject<SpriteNum>(L"Number", m_scale , m_nuber % 10, Vec3(m_pos.x+20,m_pos.y,m_pos.z));
+			m_spriteTen = GetStage()->AddGameObject<SpriteNum>(L"Number", m_scale , m_number / 10, Vec3(m_pos.x - 18, m_pos.y, m_pos.z));
+			m_spriteOne = GetStage()->AddGameObject<SpriteNum>(L"Number", m_scale, m_number % 10, Vec3(m_pos.x + 18, m_pos.y, m_pos.z));
 		}
 	}
 	void NuberManager::OnUpdate()
-	{
+	{		
+		if (!m_numLength)
+		{
+			m_spriteOne->SetNum(m_number);
+		}
+		if (m_numLength)
+		{
+			m_spriteTen->SetNum(m_number / 10);		
+			m_spriteOne->SetNum(m_number % 10);
+		}
 
+	}
+
+	void NuberManager::SetNum(int number)
+	{
+		m_number = number;
 	}
 }
 //end basecross
--- GameSources/NumberManager.h
+++ GameSources/NumberManager.h
@@ -10,19 +10,24 @@ namespace basecross{
 
 	class NuberManager :public GameObject
 	{
-		int m_nuber;
+		int m_number;
+		bool m_numLength;//1桁にするか2桁にするか
 
 		Vec3 m_pos;
 		Vec2 m_scale;
 
-		shared_ptr<SpriteNum> m_sprite;
+		shared_ptr<SpriteNum> m_spriteOne;//一の位
+		shared_ptr<SpriteNum> m_spriteTen;//十の位
 
 	public:
-		NuberManager(shared_ptr<Stage>& stage,int nuber,Vec2 scale,Vec3 pos);
+		NuberManager(shared_ptr<Stage>& stage, int number, Vec2 scale, Vec3 pos, bool numLength);
+		NuberManager(shared_ptr<Stage>& stage, int number, Vec2 scale, Vec3 pos);
 		~NuberManager();
 
 		void OnCreate()override;//作成
 		void OnUpdate()override;//更新
+
+		void SetNum(int number);//セッター
 
 	};
 
