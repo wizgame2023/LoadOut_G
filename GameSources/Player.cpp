fd94588ae68ca8362e193656908493a144a33b09
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-01-27 15:50:16 +0900

    キーボード処理の削除もれ修正

--- GameSources/Player.cpp
+++ GameSources/Player.cpp
@@ -129,7 +129,7 @@ namespace basecross{
 		{
 			if (selNow == 4)//今いる床がハッチなら
 			{
-				if (m_controler.wPressedButtons & XINPUT_GAMEPAD_B||keyState.m_bLastKeyTbl[VK_SPACE])//Bボタンを押したとき
+				if (m_controler.wPressedButtons & XINPUT_GAMEPAD_B)//Bボタンを押したとき
 				{
 					mapManager->MapDataUpdate(pos, 5);//脱出状態にする
 
@@ -318,7 +318,7 @@ namespace basecross{
 		auto mapManager = GetStage()->GetSharedGameObject<MapManager>(L"MapManager");//マップマネージャー取得
 		auto keyState = App::GetApp()->GetInputDevice().GetKeyState();//キーボードデバック用
 
-		if (m_controler.wPressedButtons & XINPUT_GAMEPAD_B || keyState.m_bPushKeyTbl[VK_SPACE])//Bボタンを押したとき
+		if (m_controler.wPressedButtons & XINPUT_GAMEPAD_B)//Bボタンを押したとき
 		{
 			if (mapManager->SelMapNow(pos) == 1)//もし、現在いるセル座標がマンホールなら
 			{
@@ -354,7 +354,7 @@ namespace basecross{
 		if (m_itemCount >= 1)//カウントが１以上なら
 		{
 			auto device = App::GetApp()->GetInputDevice().GetControlerVec();
-			if (m_controler.wPressedButtons & XINPUT_GAMEPAD_B||keyState.m_bPushKeyTbl[VK_SPACE])//Bボタンを押したとき
+			if (m_controler.wPressedButtons & XINPUT_GAMEPAD_B)//Bボタンを押したとき
 			{
 				if (mapManager->SelMapNow(pos) == 1)//もし、現在いるセル座標がマンホールの上ならば
 				{
