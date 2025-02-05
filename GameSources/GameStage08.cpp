ad1ce16b4460b5b90129431fd77428f394017521
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-02-05 10:07:28 +0900

    デバック処理の追加

--- GameSources/GameStage08.cpp
+++ GameSources/GameStage08.cpp
@@ -96,6 +96,17 @@ namespace basecross {
 
 	void GameStage08::OnUpdate()
 	{
+		//////デバック用
+		//wstringstream wss(L"");
+		//auto scene = App::GetApp()->GetScene<Scene>();
+		////auto gameStage = scene->GetGameStage();
+
+		//auto delta = App::GetApp()->GetElapsedTime();
+		//wss /* << L"デバッグ用文字列 "*/
+		//	<< L"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nFPS:" << 1.0f / delta
+		//	<< endl;
+
+		//scene->SetDebugString(wss.str());
 	}
 
 	//Player生成
